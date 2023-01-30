de3027e4afe17796f4c2b0e0325169ea3f931fed
Borislav <bora1milos@yahoo.com>
2023-01-30 23:01:31 +0100

    models

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    #host =
+    #topic =
+    name = models.CharField(max_length = 32)
+    description = models.TextField(null=True, blank=True)
+    #participants = 
+    update = models.DateTimeField(auto_now=True)
+    create = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return str(self.name)
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Room
 
 rooms = [
     {'id':1, 'name' : 'let learn python'},
     {'id':2, 'name' : 'design'},
     {'id':3, 'name' : 'front en developers'},
 ]
-
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     contex =  {'rooms': rooms }
-    return render(request, 'home.html', contex)
+    return render(request, 'base/home.html', contex)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    #rooms = Room.objects.all()
+    print (rooms)
+    room = None
+    for i in rooms:
+        print (pk,  i['id'])
+        if int(pk) == i['id']:
+            room = i
+    
+
+    contex = { 'room' : room }
+    print (contex)
+    return render(request, 'base/room.html', contex)
