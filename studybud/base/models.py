e54376a0eca8d6e377ea6312852e8a7c10f6d0c4
Borislav Zivanov <borislav.zivanov@certec.at>
2023-01-31 13:29:45 +0100

    delete item

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,14 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length = 32)
+    
+    def __str__(self):
+        return str(self.name)
 
 # Create your models here.
 class Room(models.Model):
-    #host =
-    #topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length = 32)
     description = models.TextField(null=True, blank=True)
     #participants = 
     update = models.DateTimeField(auto_now=True)
+    create = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-update', '-create']
+
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    update = models.DateTimeField(auto_now=True)
     create = models.DateTimeField(auto_created=True)
 
     def __str__(self):
-        return str(self.name)
+        return str(self.body[0:50])
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
-
+from .forms import RoomForm
 rooms = [
     {'id':1, 'name' : 'let learn python'},
     {'id':2, 'name' : 'design'},
@@ -13,15 +13,41 @@ def home(request):
     return render(request, 'base/home.html', contex)
 
 def room(request, pk):
-    #rooms = Room.objects.all()
-    print (rooms)
-    room = None
-    for i in rooms:
-        print (pk,  i['id'])
-        if int(pk) == i['id']:
-            room = i
-    
-
+    room = Room.objects.get(id=pk)
     contex = { 'room' : room }
-    print (contex)
     return render(request, 'base/room.html', contex)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form' : form}
+    return render(request, "base/room-form.html", context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    contex = { 'form' : form }
+    return render(request, "base/room-form.html", contex)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    
+    contex = { 'obj' : room }
+    return render(request, "base/delete.html", contex)
+
+
+
