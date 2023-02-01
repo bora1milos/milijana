ef2948ee380e1120cc8fd189bd67344589f287c2
Borislav Zivanov <borislav.zivanov@certec.at>
2023-02-01 21:48:44 +0100

    login

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 from .models import Room, Topic
 from .forms import RoomForm
-rooms = [
-    {'id':1, 'name' : 'let learn python'},
-    {'id':2, 'name' : 'design'},
-    {'id':3, 'name' : 'front en developers'},
-]
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+            password = User.objects.get(password=password)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user != None:
+            login(request, user)
+            return redirect('home')
+        else:
+             messages.error(request, 'User and password does not exist')
+
+    contect = {}
+    return render(request, 'base/login_register.html', contect)
+
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter( 
+        Q(topic__name__icontains = q) |
+        Q(name__icontains = q) |
+        Q(description__icontains = q)
+    )
+
+    room_counts = rooms.count
     topics = Topic.objects.all()
-    contex =  {'rooms': rooms, 'topics' : topics }
+    contex =  {'rooms': rooms, 'topics' : topics, 'room_counts' : room_counts }
     return render(request, 'base/home.html', contex)
 
 def room(request, pk):
